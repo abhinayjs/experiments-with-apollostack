5663914d288a7b98d4212f6679ac075bde9c8abf
Abhinay Jangati <abhinayjangati@carfax.com>
2019-02-08 14:25:58 -0600

    Updated Launch datasource with get by Id functions

--- server/src/datasources/launch.js
+++ server/src/datasources/launch.js
@@ -28,6 +28,17 @@ class LaunchAPI extends RESTDataSource {
           },
         };
       }
+
+      async getLaunchById({lainchId}) {
+        const response = await this.get('launches', { flight_number: launchId });
+        return this.launchReducer(response[0]);
+      }
+
+      getLaunchByIds({launchIds}) {
+          return Promise.all(
+              launchIds.map(launchId => this.getLaunchById({launchId})),
+          );
+      }
 }
 
 module.exports = LaunchAPI;
